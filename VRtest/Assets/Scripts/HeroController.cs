2a364b4fa6a7db8f3fb3d17ee8b3add681b8d59a
Paws <1436790751@qq.com>
2017-12-31 03:04:54 +0800

    人物动画调试

---
+++ VRtest/Assets/Scripts/HeroController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour {
+
+    public Animator animator;
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Alpha1)) {
+            animator.SetBool("walking", true);
+        }
+        else {
+            animator.SetBool("walking", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            animator.SetBool("shouldJump", true);
+        }
+        else {
+            animator.SetBool("shouldJump", false);
+        }
+    }
+}
